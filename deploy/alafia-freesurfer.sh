c7748daafc846a7ab434c1f63282885e476ded97
Patrick D. Lloyd <patrick@alafia.ai>
2023-11-08 12:55:42 -0800

    created build dir with Dockerfile & license, and deploy dir with installation files

--- deploy/alafia-freesurfer.sh
+++ deploy/alafia-freesurfer.sh
@@ -6,5 +6,5 @@ docker run \
 	--rm \
 	--platform=amd64 \
 	--volume=/opt/alafia-ai/alafia-freesurfer/alafia-freesurfer.license:/alafia-freesurfer.license \
-	--hostname="freesurfer-7.4.1" \
-	freesurfer/freesurfer:7.4.1
+	--hostname="alafia-freesurfer" \
+	alafia-freesurfer:latest
